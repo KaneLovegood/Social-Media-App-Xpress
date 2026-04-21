a0504f943f47b044ad573f09c50a00e3794dcb01
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2026-04-21 12:43:07 +0700

    feat: Implement Forgot Password functionality with OTP verification

--- xpress-frontend/capacitor.config.ts
+++ xpress-frontend/capacitor.config.ts
@@ -8,12 +8,10 @@ const config: CapacitorConfig = {
   // sẽ chặn hoàn toàn navigator.mediaDevices.getUserMedia
   // => Để test được video call, bạn hãy comment block `server` này lại (để app tự buil local dạng http://localhost) 
   // HOẶC dùng ngrok để cấp proxy HTTPS cho port 3001.
-  /*
   server: {
     url: 'http://192.168.0.124:3001',
     cleartext: true
   },
-  */
   plugins: {
     Media: {
       androidGalleryMode: true
